15a3ef630f0d08814297f4b96a03a079c7fd9604
Volodymyr Pekur <blackrama13@Volodymyr.local>
2020-11-04 02:05:18 +0200

    add cart overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,30 @@
-$(document).ready(function(){
+document.addEventListener("DOMContentLoaded", function(event) {
+const header_salebox = document.querySelector('.header-salebox');
+  if(header_salebox !== null) {
+    header_salebox.querySelector('.header-salebox_close').onclick = (e) =>{
+      e.preventDefault();
+      header_salebox.classList.remove('active');
+    }
+    header_salebox.onclick= (e)=>{
+      if(e.target.classList.contains('header-salebox')) {
+        e.target.classList.remove('active');
+    }
+    };
+  }
 
+const header_checkout = document.querySelector('#header_checkout');
+if(header_checkout !== null) {
+  header_checkout.onclick = (e)=> {
+    e.preventDefault();
+    header_salebox.classList.add('active');
+  }
+}
 
 
 
 
   console.log("%cDOM loaded, @blackrama13", "color: #3e5a86; font-size:10px;background:#020819;padding:10px;border-radius:5px;");
-})
+});
 
 
 
